9cb209ff2225e01d09b90aeb461fa958db4a34bf
AtesiT <atesit@mail.ru>
2026-04-05 16:21:12 +0300

    MVC Example

--- GreetingApp/ViewController.swift
+++ GreetingApp/ViewController.swift
@@ -4,12 +4,16 @@ final class ViewController: UIViewController {
 
     @IBOutlet var greetingLabel: UILabel!
     
+    private var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        person = Person(name: "Timc", surname: "Cook")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
+        greetingLabel.text = "Hello, \(person.name) \(person.surname)!"
     }
 
 }

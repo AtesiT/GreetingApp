86049ac43f84260d529cc826704ea3db3fc5c67b
AtesiT <atesit@mail.ru>
2026-04-05 15:46:20 +0300

    Initial Commit

---
+++ GreetingApp/ViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+final class ViewController: UIViewController {
+
+    @IBOutlet var greetingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
+
+}
+

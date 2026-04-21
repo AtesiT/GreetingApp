1f0cdce0796a66218ff44c7f139e2342423e4c18
AtesiT <atesit@mail.ru>
2026-04-21 18:06:45 +0300

    ADD: GreetingInteractor (added two protocols, func provideGreetingData (with datas))

--- GreetingApp/Greeting/GreetingInteractor.swift
+++ GreetingApp/Greeting/GreetingInteractor.swift
@@ -1,1 +1,27 @@
 import Foundation
+
+protocol GreetingInteractorInputProtocol {
+    init(presenter: GreetingInteractorOutputProtocol)
+    func provideGreetingData()
+}
+
+protocol GreetingInteractorOutputProtocol: AnyObject {
+    func receiveGreetingData(greetingData: GreetingData)
+}
+
+class GreetingInteractor: GreetingInteractorInputProtocol {
+    unowned private let presenter: GreetingInteractorOutputProtocol
+    
+    required init(presenter: any GreetingInteractorOutputProtocol) {
+        self.presenter = presenter
+    }
+    
+    func provideGreetingData() {
+        let person = Person(name: "Tim", surname: "Cook")
+        let greetingData = GreetingData(name: person.name, surname: person.surname)
+        presenter.receiveGreetingData(greetingData: greetingData)
+        
+    }
+    
+    
+}
